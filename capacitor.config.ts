b5325ac12b45525f6e90c40d9a78a53b192529c0
Michał Dacko <michaldacko4@wp.pl>
2024-07-01 15:22:22 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'campi-app',
+  webDir: 'dist'
+};
+
+export default config;
